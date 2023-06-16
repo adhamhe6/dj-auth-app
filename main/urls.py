39706ea0b9bf70e68b8186da659c6fc865068cde
adham-66 <adhamhewalla66@gmail.com>
2023-06-16 14:44:54 +0300

    Add create_post url & its url

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('sign-up/', views.sign_up, name='sign_up'),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('create-post', views.create_post, name='create_post'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from .forms import RegisterForm
+from .forms import RegisterForm, PostForm
 from django.contrib.auth import login
+from .models import Post
 
 def home(request):
     
@@ -9,8 +10,17 @@ def home(request):
 
 
 def create_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect(reverse('home'))
+    else:
+        form = PostForm()
         
-    return render(request, "main/create_post.html")
+    return render(request, "main/create_post.html", {'form':form})
 
 def sign_up(request):
     if request.method == 'POST':
