bd19fcf800a33a13d40093d50611ad533385eea0
adham-66 <adhamhewalla66@gmail.com>
2023-06-16 14:55:22 +0300

    Add home view

--- main/views.py
+++ main/views.py
@@ -2,11 +2,32 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from .forms import RegisterForm, PostForm
 from django.contrib.auth import login
+from django.contrib.auth.models import User, Group
 from .models import Post
 
 def home(request):
-    
-    return render(request, 'main/home.html')
+    posts = Post.objects.all()
+    if request.method == "POST":
+        post_id = request.POST.get("post-id")
+        user_id = request.POST.get("user-id")
+        if post_id:
+            post = Post.objects.filter(id=post_id).first()
+            if post and (post.author == request.user or request.user.has_perm("main.delete_post")):
+                post.delete()
+        elif user_id:
+            user = User.objects.filter(id=user_id).first()
+            if user and request.user.is_staff:
+                try:
+                    group = Group.objects.get(name='default')
+                    group.user_set.remove(user)
+                except:
+                    pass
+                try:
+                    group = Group.objects.get(name='mod')
+                    group.user_set.remove(user)
+                except:
+                    pass
+    return render(request, 'main/home.html', {'posts':posts})
 
 
 def create_post(request):
