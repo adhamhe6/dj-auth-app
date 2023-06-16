4553ddf6404937fecdeedc8da825af2a5dd91a5a
adham-66 <adhamhewalla66@gmail.com>
2023-06-16 14:25:00 +0300

    registration
    Add sign_up view, django auth urls and templates

--- main/views.py
+++ main/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .forms import RegisterForm
+from django.contrib.auth import login
 
-# Create your views here.
+def home(request):
+    
+    return render(request, 'main/home.html')
+
+
+def create_post(request):
+        
+    return render(request, "main/create_post.html")
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(reverse('home'))
+    else:
+        form = RegisterForm()
+    return render(request, 'registration/sign_up.html', {'form':form})
--- project/settings.py
+++ project/settings.py
@@ -127,3 +127,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER =  "EMAIL"
+EMAIL_HOST_PASSWORD = "PASS"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+LOGIN_REDIRECT_URL = "/home"
+LOGOUT_REDIRECT_URL = "/accounts/login"
